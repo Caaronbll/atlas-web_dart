0c4180534a4ced7a66c70ac25a14029c39ff31e0
Caaronbll <aaronbll46@gmail.com>
2024-06-14 17:09:25 -0500

    task 4

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,12 @@
+void outer(String name, String id) {
+  
+  String inner() {
+    List<String> fullNameList = name.split(' ');
+    String firstName = fullNameList[0];
+    String lastName = fullNameList[1];
+    String alias = lastName[0] + '. ' + firstName;
+    return 'Hello Agent $alias your id is $id';
+  }
+
+  print(inner());
+}
