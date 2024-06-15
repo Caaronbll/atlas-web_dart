fd94628ea8723241d2d298abff56f6f7df2e7ca2
Caaronbll <aaronbll46@gmail.com>
2024-06-15 12:32:09 -0500

    task 7

---
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -0,0 +1,19 @@
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+  // total points for team A
+  int teamAPoints = (teamA['Free throws'] ?? 0) * 1 +
+                    (teamA['2 pointers'] ?? 0) * 2 +
+                    (teamA['3 pointers'] ?? 0) * 3;
+
+  // total points for team B
+  int teamBPoints = (teamB['Free throws'] ?? 0) * 1 +
+                    (teamB['2 pointers'] ?? 0) * 2 +
+                    (teamB['3 pointers'] ?? 0) * 3;
+
+  if (teamAPoints > teamBPoints) {
+    return 1;
+  } else if (teamAPoints < teamBPoints) {
+    return 2;
+  } else {
+    return 0;
+  }
+}
