442140aafabcc434b8a0cb91df198d9a622c67d0
Caaronbll <aaronbll46@gmail.com>
2024-06-17 17:05:23 -0500

    task 10

---
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -0,0 +1,20 @@
+import '9-palindrome.dart';
+
+String longestPalindrome(String s) {
+  if (s.length < 3) {
+    return "none";
+  }
+
+  String longestPal = "none";
+
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i + 3; j <= s.length; j++) {
+      String substring = s.substring(i, j);
+      if (isPalindrome(substring) && substring.length > longestPal.length) {
+        longestPal = substring;
+      }
+    }
+  }
+
+  return longestPal;
+}
