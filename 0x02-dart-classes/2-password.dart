48a457dfebb215d878a87cb1af8fd977605c31dc
Caaronbll <aaronbll46@gmail.com>
2024-06-18 18:35:55 -0500

    task 2

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,20 @@
+class Password {
+  String password = '';
+
+  bool isValid() {
+    if (password.length < 8 || password.length > 16) {
+      return false;
+    }
+    bool hasUpperCase = password.contains(RegExp(r'[A-Z]'));
+    bool hasLowerCase = password.contains(RegExp(r'[a-z]'));
+    bool hasDigits = password.contains(RegExp(r'[0-9]'));
+
+    return hasUpperCase && hasLowerCase && hasDigits;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
+
+}
