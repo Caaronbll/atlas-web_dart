12f0ef7e092a45adb9e3748f043b2ded528b7d93
Caaronbll <aaronbll46@gmail.com>
2024-06-25 12:30:38 -0500

    task 3

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,32 @@
+import '3-util.dart';
+import 'dart:convert';
+
+Future<String> greetUser() async {
+  try {
+    String user = await fetchUserData();
+    String userName = jsonDecode(user)['username'];
+    return 'Hello $userName';
+  }
+  catch (error) {
+    return 'error caught: $error';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    bool check = await checkCredentials();
+
+    if (check) {
+      print('There is a user: true');
+      return await greetUser();
+    }
+    else {
+      print('There is a user: false');
+      return 'Wrong credentials';
+    }
+  }
+  catch(error) {
+    return 'error caught: $error';
+  }
+
+}
